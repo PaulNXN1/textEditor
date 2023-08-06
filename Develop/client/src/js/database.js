d79a82636cf16c614a15db09c1a4e8ae234d2482
Paul Nixon <paulnxn1@gmail.com>
2023-08-05 22:06:53 -0400

    Database.js - variables updated

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -21,24 +21,37 @@ const initdb = async () =>
 
 
 // TODO - for the student : Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => { 
-  
+
+export const putDb = async (content) => {
+
   console.error('putDb not implemented');
 
   const jateDb = await openDB('jate', 1);
   const text = jateDb.transaction('jate', 'readwrite');
   const store = text.objectStore('jate');
-  const requestUpdate = store.put({id: 1, value: content});
-   const finalResult = await requestUpdate;
+  const requestUpdate = store.put({ id: 1, value: content });
+  const finalResult = await requestUpdate;
 
-   // Console log confirmation message //
+  // Console log confirmation message //
 
   console.log('Super - all data saved.', finalResult);
 
 }
 
 // TODO - for the student : Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const text = jateDb.transaction('jate', 'readonly');
+  const store = text.objectStore('jate');
+  const requestUpdate = store.getAllKeys();
+  const finalResult = await requestUpdate;
+  console.log('result.value', finalResult.value);
+
+
+  return finalResult.value;
+};
 
 
 // Callback f(x)
