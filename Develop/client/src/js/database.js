322b8930d1565e4596b384e37cad73bb6a38d545
Paul Nixon <paulnxn1@gmail.com>
2023-08-07 13:14:45 -0400

    webpack.config coding

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -54,6 +54,6 @@ export const getDb = async () => {
 };
 
 
-// Callback f(x)
+// Callback / running f(x)
 
 initdb();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -21,13 +21,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
+
+      // Adding our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Another Text Editor!',
+        background_color: '#272822',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
     ],
 
+    // CSS loading & Babel-Loader implementation 
     module: {
-      rules: [
-        
+      rules: [{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        // We use babel-loader in order to use ES6.
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      }
       ],
     },
   };
 };
+
+
